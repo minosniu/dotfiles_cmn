dc91ae575b2f989544fd71e0a7eebb891220df1d
C. Minos Niu <minos.niu@gmail.com>
2012-07-18 23:26:15 -0700

    snapsho
    Need a function that package-install libraries if necessary

--- emacs.d/cmn.el
+++ emacs.d/cmn.el
@@ -35,7 +35,30 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
 
+(defvar prelude-packages
+  '(ack-and-a-half auctex coffee-mode gist haskell-mode inf-ruby
+                   magit magithub markdown-mode paredit python
+                   solarized-theme )
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
+(package-initialize)
+;;; prelude-packages.el ends here
 
 (vendor 'vimpulse)

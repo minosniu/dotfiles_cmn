1479361ccdba45963db39df914c97fe32c0b8531
C. Minos Niu <minos.niu@gmail.com>
2016-06-14 07:40:44 +0800

    Stop tracking emacs configs.

--- emacs.d/package-configs/conf-goto-last-change.el
+++
@@ -1,6 +0,0 @@
-(:name goto-last-change		; move pointer back to last change
-       :after (progn
-           ;; when using AZERTY keyboard, consider C-x C-_
-           (global-set-key (kbd "C-x C-/") 'goto-last-change)))
-
-
--- emacs.d/package-configs/conf-smex.el
+++
@@ -1,6 +0,0 @@
-(:name smex				; a better (ido like) M-x
-       :after (progn
-              (setq smex-save-file "~/.emacs.d/.smex-items")
-	      (global-set-key (kbd "M-x") 'smex)
-	      (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-          ))
